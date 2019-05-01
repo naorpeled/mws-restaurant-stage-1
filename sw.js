1b35ff7bfe5173553db38ed66af24ec005b51841
Naor <thehecticbyte@gmail.com>
2019-05-01 22:17:34 +0300

    fix: Changed the list of URLs to cache - Some of the pages weren't saved due to wrong URLs

--- sw.js
+++ sw.js
@@ -1,26 +1,26 @@
-let cacheVersion = 'nrestaurants-static-v2';
+let cacheVersion = 'nrestaurants-static-v4';
 
 // Save all the files into the cache on every Service Worker install
 self.addEventListener('install', function(event){
 const urlsToCache = [
   '/',
-  'js/dbhelper.js',
-  'js/main.js',
-  'js/restaurant_info.js',
-  'img/1.jpg',
-  'img/2.jpg',
-  'img/3.jpg',
-  'img/4.jpg',
-  'img/5.jpg',
-  'img/6.jpg',
-  'img/7.jpg',
-  'img/8.jpg',
-  'img/9.jpg',
-  'img/10.jpg',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg',
   'index.html',
   'restaurant.html',
-  'data/restaurants.json',
-  'css/styles.css'
+  '/data/restaurants.json',
+  '/css/styles.css'
 ];
 
 event.waitUntil(
